c759b84837c457833f65b4071690b07badbaa33a
takako.ishii <lifedesign.chris@gmail.com>
2020-09-29 03:13:53 +0000

    add-p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,15 @@
         @include('tasks.index')
            
     @else
-        <div class="center jumbotron">
+        <div class="center jumbotron ">
             <div class="text-center">
                 <h1>Welcome to the Tasklist</h1>
                 {{-- ユーザ登録ページへのリンク --}}
-                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-info']) !!}
             </div>
+        </div>
+        <div class="text-center">
+            <p class="text-success">You can use this app to manage your tasks!</p>
         </div>
     @endif
 @endsection
